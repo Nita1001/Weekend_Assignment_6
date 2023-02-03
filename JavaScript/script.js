baa173a5da4261796b59c048d690b9a34d4af3e2
Nita1001 <devinedevops@gmail.com>
2023-02-03 11:43:02 +0200

    game board and inventory

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,11 +1,50 @@
-
+const TOTAL_TILES = 144;
+const INV_TILES = 6;
 let container = document.querySelector('#tiles-container');
-const TOTAL_TILES = 120;
+const inventoryTileTiles = document.querySelector('#inv-grid');
+const inventoryToolTiles = document.querySelector('#tools-grid');
 
-for(let i = 0; i < TOTAL_TILES; i++)
-{
-    const div = document.createElement('div');
-    container.appendChild(div);
-    div.classList.add('tile');
-    div.setAttribute('id', `tile${i}`);
+
+function createGameBoard(){
+
+    for(let i = 0; i < TOTAL_TILES; i++)
+    {
+        const div = document.createElement('div');
+        container.appendChild(div);
+        div.classList.add('tile');
+        div.classList.add('dark-tile');
+        div.classList.add('tile-border');
+        div.setAttribute('id', `tile${i}`);
+    }
+
+    // inventory - tiles and tools
+    for(let i = 0; i < INV_TILES; i++)
+    {
+        const div = document.createElement('div');
+        inventoryTileTiles.appendChild(div);
+        div.classList.add('tile');
+        div.classList.add('dark-tile');
+        div.classList.add('tile-border');
+        div.setAttribute('id', `tile${i}`);
+    }
+    for(let i = 0; i < INV_TILES; i++)
+    {
+        const div = document.createElement('div');
+        inventoryToolTiles.appendChild(div);
+        div.classList.add('tile');
+        div.classList.add('dark-tile');
+        div.classList.add('tile-border');
+        div.setAttribute('id', `tile${i}`);
+    }
+
 }
+
+
+
+function sky(){}
+function dirt(){}
+
+createGameBoard();
+sky();
+dirt();
+
